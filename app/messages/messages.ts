f52faa84ba015d269cad5bf80ec9ec2b4038de43
Kapitar <kkapitar@ya.ru>
2026-01-02 14:43:54 +0900

    fix: add tristan photo

--- app/messages/messages.ts
+++ app/messages/messages.ts
@@ -4,5 +4,5 @@ export const messages = [
     {author: 'Dennis', text: 'The big ONE NINE. Another year is ticking closer to twenty😱. My beautiful Fuzhounese bro. Wishing you the best birthday!', 'image': 'dennis.png'},
     {author: 'Hiro', text: 'Hello big boy. Happy 19th birthday!! As im writing this i think ill be seeing you like in a day or two in the city. Lets lock in together again next semester twin. From your fav coach <3', 'image': 'hiro.png'},
     {author: 'Noah', text: 'Happy birthday Brendiddy 😍hope u have a good bday gng and can’t wait to meet up again on campus! We are getting big this sem lfg bro and hope it’s just as fun', 'image': 'noah.png'},
-    {author: 'Tristan', text: 'Hello unc. Congrats on another rotation, cakes and candles fr. 🫃', 'image': 'tristan.png'}
+    {author: 'Tristan', text: 'Hello unc. Congrats on another rotation, cakes and candles fr. 🫃', 'image': 'tristan.jpg'}
 ]as {author: string, text: string, image?: string}[]
