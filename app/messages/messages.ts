4004958bd695bc0f0e8466915b110a36b585cc42
Hiro <hryk.sakuma@gmail.com>
2026-01-02 00:46:19 -0500

    update hiro

--- app/messages/messages.ts
+++ app/messages/messages.ts
@@ -1,8 +1,32 @@
 export const messages = [
-    {author: 'Artem', text: 'Heyyy brendan!! Happy birthdayyyyyy!! I am so grateful I had a chance to become friends with you this year. You are a very lovely and handsome person. Hope you have an amazing day!', 'image': 'artem.png'},
-    {author: 'Lily', text: 'Happy 19th birthday! Wow you’re so old now. Thank you for being my friend and watching me crash out every exam :D Though we can’t meet up physically, I hope you have the best day ever :3', 'image': 'lily.png'},
-    {author: 'Dennis', text: 'The big ONE NINE. Another year is ticking closer to twenty😱. My beautiful Fuzhounese bro. Wishing you the best birthday!', 'image': 'dennis.png'},
-    {author: 'Hiro', text: 'Hello big boy. Happy 19th birthday!! As im writing this i think ill be seeing you like in a day or two in the city. Lets lock in together again next semester twin. From your fav coach <3', 'image': 'hiro.png'},
-    {author: 'Noah', text: 'Happy birthday Brendiddy 😍hope u have a good bday gng and can’t wait to meet up again on campus! We are getting big this sem lfg bro and hope it’s just as fun', 'image': 'noah.png'},
-    {author: 'Tristan', text: 'Hello unc. Congrats on another rotation, cakes and candles fr. 🫃', 'image': 'tristan.png'}
-]as {author: string, text: string, image?: string}[]
+	{
+		author: "Artem",
+		text: "Heyyy brendan!! Happy birthdayyyyyy!! I am so grateful I had a chance to become friends with you this year. You are a very lovely and handsome person. Hope you have an amazing day!",
+		image: "artem.png",
+	},
+	{
+		author: "Lily",
+		text: "Happy 19th birthday! Wow you’re so old now. Thank you for being my friend and watching me crash out every exam :D Though we can’t meet up physically, I hope you have the best day ever :3",
+		image: "lily.png",
+	},
+	{
+		author: "Dennis",
+		text: "The big ONE NINE. Another year is ticking closer to twenty😱. My beautiful Fuzhounese bro. Wishing you the best birthday!",
+		image: "dennis.png",
+	},
+	{
+		author: "Hiro",
+		text: "Hello big boy. Happy 19th birthday!! As im writing this i think ill be seeing you like in a day or two in the city. Lets lock in together again next semester twin. From your fav coach <3",
+		image: "hiro.jpg",
+	},
+	{
+		author: "Noah",
+		text: "Happy birthday Brendiddy 😍hope u have a good bday gng and can’t wait to meet up again on campus! We are getting big this sem lfg bro and hope it’s just as fun",
+		image: "noah.png",
+	},
+	{
+		author: "Tristan",
+		text: "Hello unc. Congrats on another rotation, cakes and candles fr. 🫃",
+		image: "tristan.png",
+	},
+] as { author: string; text: string; image?: string }[];
