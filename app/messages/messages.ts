6cd6b18dcfc2576da1812a1611a173a6e01ce20f
Hiro <103229782+burrbox@users.noreply.github.com>
2026-01-03 23:06:19 -0500

    half update messages

--- app/messages/messages.ts
+++ app/messages/messages.ts
@@ -29,4 +29,49 @@ export const messages = [
 		text: "Hello unc. Congrats on another rotation, cakes and candles fr. 🫃",
 		image: "tristan.jpg",
 	},
+	{
+		author: "Cedric",
+		text: "Happy birthday ma boii I hope you have a great 19th birthday!! I will be waiting for that Chef Lin to make us some good Chinese food when we room together next year ;) Can’t wait for all the future memories that we will make together. Hope 19 treats you well~",
+		image: "cedric.png",
+	},
+	{
+		author: "Ciana",
+		text: "HAI BRENDAN HAPPY BIRTHDAY KING!! I hope 19 treats you well and your tiktok career launches😇Glad to have another person repping the long islanders. SEE U SOON TWIN🥳🥳",
+		image: "ciana.png",
+	},
+	{
+		author: "Jason",
+		text: "Cakes and candles mponzu 🎉🥳. Thanks for being such a cool, handsome, and lwky sexy friend 🙂 We frfr need to hit up uniqlo some day together btw. Happy birthday bro!!!",
+		image: "jason.png",
+	},
+	{
+		author: "Hayagriv",
+		text: "Cakes and candles Brendiddy 🎂 🎂 🎂Welcome to the unc club 🙌 Hope you have a great bday even though we can meet in person!!! 🎉🎉",
+		image: "hayagriv.png",
+	},
+	{
+		author: "Justin",
+		text: "What’s uppp brendannn. Congrats on the unc status! Hope ur 2026 is a 10/10 :) Happy birthday!!",
+		image: "justin.jpg",
+	},
+	{
+		author: "Tyler",
+		text: "Happruay bathday Brendussy! 👅One more year till official unc status. 🤓👆🏻Cuul guy too ig, kinda glad we met 🫩✌🏻",
+		image: "IMG_2026103_230145_157.jpg",
+	},
+	{
+		author: "Winston",
+		text: "MY BOYYYY Happy birthday grown man. Missing u this break. Let’s get CM chicken ASAP. Luv ya",
+		image: "winston.png",
+	},
+	{
+		author: "Youngjun",
+		text: "Hey future roomie. Happy birthday! Wanna say thank you for making my first semester experience more fun. I wanna let you know how much of a great friend you are for all the time you made me laugh. I hope everything goes well next semester too. I hope you have a wonderful birthday and I wish you the best in every moment that’s coming.",
+		image: "youngjun.png",
+	},
+	{
+		author: "Zelin",
+		text: "Happy Birthday Brotha!, missing u fo sho. Can’t wait to see you again and we gotta run it back and hoop. Congrats on being 19. Love u gangy!",
+		image: "zelin.png",
+	},
 ] as { author: string; text: string; image?: string }[];
